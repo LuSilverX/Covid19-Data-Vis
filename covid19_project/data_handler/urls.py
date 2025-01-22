1b4982abad70389f98e2b2c7377e8ca373f5b18b
lsilva56 <lsilva56@toromail.csudh.edu>
2025-01-22 01:01:49 -0800

    added main and live_data htmls & js file w main as landing page now

--- covid19_project/data_handler/urls.py
+++ covid19_project/data_handler/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import dashboard_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),  # Main page
+    path('pandemic/', TemplateView.as_view(template_name='dashboard.html'), name='pandemic_data_page'),  # Pandemic Data
+    path('live/', TemplateView.as_view(template_name='live_data.html'), name='live_data_page'),  # Live Data
 ]
