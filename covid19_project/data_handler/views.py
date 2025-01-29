42a1a140b0ca68888642ca86c9537bbb249650d2
lsilva56 <lsilva56@toromail.csudh.edu>
2025-01-29 00:43:40 -0800

    preliminary work on 'pre_dashboard' templ'

--- covid19_project/data_handler/views.py
+++ covid19_project/data_handler/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import CovidCountyData, CovidStateData, CovidUSData
 
 # Create your views here.
-def dashboard_view(request):
-    return render(request, 'dashboard.html')
+def pre_dashboard(request):
+    county_data = CovidCountyData.objects.all()[:100]  # Limit data for performance
+    state_data = CovidStateData.objects.all()[:100]
+    us_data = CovidUSData.objects.all()[:100]
+
+    context = {
+        'county_data': county_data,
+        'state_data': state_data,
+        'us_data': us_data
+    }
+    return render(request, 'pre_dashboard.html', context)
