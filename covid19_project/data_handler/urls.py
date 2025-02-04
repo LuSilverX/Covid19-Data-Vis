ebd5a0d23526e1fae1f5ada31ff747bfa348471c
lsilva56 <lsilva56@toromail.csudh.edu>
2025-02-03 19:25:20 -0800

    working code. checkpoint

--- covid19_project/data_handler/urls.py
+++ covid19_project/data_handler/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import pre_dashboard
+from .views import pre_dashboard, get_paginated_data
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='main.html'), name='main'),  # Main page
     path('pandemic/', pre_dashboard, name='pandemic_data_page'),  # Early Data
     path('live/', TemplateView.as_view(template_name='live_data.html'), name='live_data_page'),  # Live Data
     path('covid-dashboard/', pre_dashboard, name='covid_dashboard'),
+    path('api/data/', get_paginated_data, name='get_paginated_data'),
 ]
 
 
--- covid19_project/data_handler/views.py
+++ covid19_project/data_handler/views.py
@@ -2,6 +2,65 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import CovidCountyData, CovidStateData, CovidUSData
 
+from django.http import JsonResponse
+from django.core.paginator import Paginator
+from .models import CovidCountyData, CovidStateData, CovidUSData
+
+def get_paginated_data(request):
+    # Get the page numbers from the request (default to 1)
+    county_page = int(request.GET.get('county_page', 1))
+    state_page = int(request.GET.get('state_page', 1))
+    us_page = int(request.GET.get('us_page', 1))
+
+    # Number of entries per page for each dataset
+    entries_per_page = 10
+
+    # Query all data and order by date (change ordering if desired)
+    county_queryset = CovidCountyData.objects.all().order_by('date')
+    state_queryset = CovidStateData.objects.all().order_by('date')
+    us_queryset = CovidUSData.objects.all().order_by('date')
+
+    # Create individual paginators
+    county_paginator = Paginator(county_queryset, entries_per_page)
+    state_paginator = Paginator(state_queryset, entries_per_page)
+    us_paginator = Paginator(us_queryset, entries_per_page)
+
+    # Get each requested page
+    county_page_obj = county_paginator.get_page(county_page)
+    state_page_obj = state_paginator.get_page(state_page)
+    us_page_obj = us_paginator.get_page(us_page)
+
+    # Convert each set of objects to a list of dictionaries
+    county_data = list(county_page_obj.object_list.values())
+    state_data = list(state_page_obj.object_list.values())
+    us_data = list(us_page_obj.object_list.values())
+
+    # Build and return a JSON response
+    return JsonResponse({
+        'county_data': county_data,
+        'county_pagination': {
+            'has_next': county_page_obj.has_next(),
+            'has_previous': county_page_obj.has_previous(),
+            'current_page': county_page_obj.number,
+            'total_pages': county_paginator.num_pages,
+        },
+        'state_data': state_data,
+        'state_pagination': {
+            'has_next': state_page_obj.has_next(),
+            'has_previous': state_page_obj.has_previous(),
+            'current_page': state_page_obj.number,
+            'total_pages': state_paginator.num_pages,
+        },
+        'us_data': us_data,
+        'us_pagination': {
+            'has_next': us_page_obj.has_next(),
+            'has_previous': us_page_obj.has_previous(),
+            'current_page': us_page_obj.number,
+            'total_pages': us_paginator.num_pages,
+        },
+    })
+
+
 def pre_dashboard(request):
     # Separate page numbers for each dataset
     county_page = int(request.GET.get('county_page', 1))
